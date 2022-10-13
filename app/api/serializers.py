87761f12e833a59ad5223f234808b9fb6a513284
Volodymyr Lazarev <vovalaz321@gmail.com>
2022-10-13 15:06:26 +0300

    Task 9: Added ip authentication and short url management

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -5,5 +5,5 @@ from .models import UrlShortener
 class UrlShortenerSerializer(ModelSerializer):
     class Meta:
         model = UrlShortener
-        fields = ['longurl', 'shorturl']
-        read_only_fields = ['shorturl']
+        fields = ['longurl', 'shorturl', 'user_ip_address']
+        read_only_fields = ['shorturl', 'user_ip_address']
