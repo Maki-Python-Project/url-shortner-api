8b5c33a80b428aec16765997e5e2c436090ee380
Volodymyr Lazarev <vovalaz321@gmail.com>
2022-10-26 11:17:32 +0300

    Task 20: changed views to use fastapi and repository

--- app/api/views.py
+++ app/api/views.py
@@ -1,44 +1,55 @@
+import os
+
+from fastapi import Request
+from fastapi.responses import JSONResponse, RedirectResponse
+
 from api.apps import application
-from django.conf import settings
-from django.shortcuts import redirect
-from django.http import JsonResponse
-from rest_framework.response import Response
-from django.db.models import Count
-from django.http import HttpResponseRedirect
-from typing import Type
-
 from api.models import UrlShortener
-from api.utils import get_user_ip, get_short_url
+from api.repository import PostgresRepository
+from api.utils import convert_cursor_to_dict, get_user_ip, get_short_url, get_db_connection
 
 
-async def create_shorturl(request: Response) -> Response:
-    longurl = request.POST.get('longurl')
+session = None
+repo = PostgresRepository(session)
+HOST_URL = os.environ.get('HOST_URL')
+
+
+@application.post('/shorten/')
+async def create_shorturl(url: UrlShortener, request: Request) -> JSONResponse:
+    db_connection = get_db_connection()
+    longurl = url.longurl
 
     ip = get_user_ip(request)
 
-    long_url_obj = UrlShortener.objects.filter(longurl=longurl, user_ip_address=ip)
+    long_url_obj = db_connection.get_one({'longurl': longurl, 'user_ip_address': ip})
 
     async for obj in long_url_obj:
-        return JsonResponse({'longurl': obj.longurl, 'shorturl': settings.HOST_URL + obj.shorturl})
+        return JSONResponse({'longurl': obj.longurl, 'shorturl': HOST_URL + obj.shorturl})
 
-    shorturl = await get_short_url()
+    shorturl = await get_short_url(db_connection)
 
-    await UrlShortener.objects.acreate(longurl=longurl, shorturl=shorturl, user_ip_address=ip)
-    shorturl = settings.HOST_URL + str(shorturl)
+    db_connection.add({'longurl': longurl, 'shorturl': shorturl, 'user_ip_address': ip})
+    shorturl = HOST_URL + shorturl
 
-    return JsonResponse({'longurl': longurl, 'shorturl': shorturl})
-
-
-async def redirect_shorturl(request: Response, shorturl: str) -> HttpResponseRedirect:
-    redirect_link = await UrlShortener.objects.filter(shorturl=shorturl).afirst()
-    return redirect(redirect_link.longurl)
+    return JSONResponse({'longurl': longurl, 'shorturl': shorturl})
 
 
-def get_the_most_popular(request) -> Type[UrlShortener]:
-    qs = UrlShortener.objects.values("longurl").annotate(count=Count('longurl')).order_by('-count')
-    return JsonResponse(list(qs), safe=False)
+@application.get('/shorten/{shorturl}')
+async def redirect_shorturl(shorturl: str) -> RedirectResponse:
+    db_connection = get_db_connection()
+    redirect_link = db_connection.get_one({'shorturl': shorturl})
+    return RedirectResponse(url=redirect_link.longurl, status_code=301)
 
 
-def get_count_all_shortened_url(request: Response) -> JsonResponse:
-    data = UrlShortener.objects.all().count()
-    return JsonResponse({'count': data})
+@application.get('/the-most-popular/')
+def get_the_most_popular() -> UrlShortener:
+    db_connection = get_db_connection()
+    data = db_connection.annotate({'$group': {'_id': '$longurl', 'count': {'$sum': 1}}}, {'$sort': {'count': -1}})
+    return JSONResponse(convert_cursor_to_dict(data))
+
+
+@application.get('/shortened-urls-count/')
+def get_count_all_shortened_url() -> JSONResponse:
+    db_connection = get_db_connection()
+    data = db_connection.count()
+    return JSONResponse({'count': data})
