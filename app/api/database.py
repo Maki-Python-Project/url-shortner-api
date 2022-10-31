b4fc5ed2ed30eff03a8693456f6f5aae799b01f4
Volodymyr Lazarev <vovalaz321@gmail.com>
2022-10-31 11:44:57 +0200

    Task 23: refactored hardcoded sql url and added support of fastapi for prod

--- app/api/database.py
+++ app/api/database.py
@@ -1,13 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db:5432/url_shortener_dev"
+SQLALCHEMY_DATABASE_URL = os.environ.get('SQL_URI')
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
